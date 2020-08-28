8033e0c905c6e6da5f3d9eb98d1ab0353f52b60f
chenjing <chenjing@yscredit.com>
2020-08-28 15:33:36 +0800

    main

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-
+import {http} from './common/http.js'
+import { CONSTANT } from './common/constant.js'
 Vue.config.productionTip = false
-
+Vue.prototype.$ysHttp = http;
+Vue.prototype.$constant = CONSTANT;
 App.mpType = 'app'
 
 const app = new Vue({
